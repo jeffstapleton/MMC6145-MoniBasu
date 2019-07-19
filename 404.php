a1faa179bd0bdbba9ba37ceecf4c0aaba1468db4
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-19 14:58:01 -0500

    Create div 'error' in 404 page

--- 404.php
+++ 404.php
@@ -1,19 +1,20 @@
 <?php /*Template Name: 404 Page */
 
 get_header(); ?>
-
+<div class="error">
 <div class="container">
   <div class="four-background">
     <div class="row four-info">
       <div class="twelve columns">
 
-        <h2 class="center">Page Not Written</h2>
+        <h2 class="center">Whoops?!</h2>
         <h3 class="center">Let me know that you want to hear more about this</h3>
         <p class="center">Go back to my <a href="<?php echo home_url('/'); ?>">home page</a></p>
 
       </div>
     </div>
   </div>
+</div>
 </div>
 
 
