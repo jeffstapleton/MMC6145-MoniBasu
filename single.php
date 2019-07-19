cfbc2da89368500e50a3f2a37e6032df580b7399
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-19 17:06:01 -0500

    Move image out of contianer & label sections single.php

--- single.php
+++ single.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
-<div class="container"><?php
+
+<?php
   if(have_posts()){
     while(have_posts()){
       the_post(); ?>
-      <h2 class="post-title"><?php the_title(); ?></h2>
-      <p class="pub-info post-date"><?php echo "Published: " . get_the_date(); ?></p>
+      
+<!-- Hero Image -->      
       <div class="post-feature">
         <?php the_post_thumbnail(); ?>
       </div>
+
+<!-- Title & Meta -->      
+      <div class="container">
+      <h2 class="post-title"><?php the_title(); ?></h2>
+      <p class="pub-info post-date"><?php echo "Published: " . get_the_date(); ?></p>
+
+<!-- Content Start -->
+      
       <?php the_content(); ?>
       <p><?php echo "This post written by : " . get_the_author(); ?></p>
 <?php    }//end of while loop
