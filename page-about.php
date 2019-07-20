39ac29a494315c5e03a71f5684bd3c4f9e6db813
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-20 15:17:18 -0500

    Add red block in page-about.php

--- page-about.php
+++ page-about.php
@@ -2,53 +2,62 @@
 
 <?php get_header(); ?>
 
-  <!-- HERO SECTION -->
-  <div id="hero">
-    <div class="row">
-      <div class="twelve columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post(); ?>
-              <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
-              <?php
-            }
-          }
-        ?>
-        
+<!-- HERO SECTION -->
+<div id="hero">
+  <div class="row">
+    <div class="twelve columns">
+      <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+          <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
+          <?php
+        }
+      }
+      ?>
+      
+    </div>
+  </div>
+</div>
+
+<!-- Content Start -->
+<div class="container featured-block"> 
+  <div class="row float-top">
+    <div class="twelve columns float-top">
+      <div class="featured-block-image">
+        <h2 class="white">YEP YEP YEP </h2>
       </div>
     </div>
   </div>
-  
-  <!-- Content Start -->
-  <div class="container">
-    <div class="row">
-      <div class="three columns responsive">
-          <?php dynamic_sidebar('left-sidebar'); ?>
-          <h3>Contact Me</h3>
-            <a class="social-link email" href="mailo:moni@monibasu.com">Email Me</a>
-            <a class="social-link facebook" href="https://www.facebook.com/WriterMoni/">Facebook</a>
-            <a class="social-link twitter" href="https://twitter.com/TheMoniBasu">Twitter</a>
-            <a class="social-link instagram" href="https://www.instagram.com/EvilReporterChick/">Instagram</a>
-            <a class="social-link linkedin" href="https://www.linkedin.com/in/monibasu/">LinkedIn</a>
-          
-        </div> 
-
-      <div class="nine columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post(); ?>
-              <h2><?php the_title(); ?></h2>
-              <?php the_content();
-            }
-          }
-        ?>
-      </div>
-
-
+</div>
+<div class="container">
+  <div class="row">
+    <div class="three columns responsive">
+      <?php dynamic_sidebar('left-sidebar'); ?>
+      <h3>Contact Me</h3>
+      <a class="social-link email" href="mailo:moni@monibasu.com">Email Me</a>
+      <a class="social-link facebook" href="https://www.facebook.com/WriterMoni/">Facebook</a>
+      <a class="social-link twitter" href="https://twitter.com/TheMoniBasu">Twitter</a>
+      <a class="social-link instagram" href="https://www.instagram.com/EvilReporterChick/">Instagram</a>
+      <a class="social-link linkedin" href="https://www.linkedin.com/in/monibasu/">LinkedIn</a>
+      
+    </div> 
+    
+    <div class="nine columns">
+      <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+          <h2><?php the_title(); ?></h2>
+          <?php the_content();
+        }
+      }
+      ?>
     </div>
+    
+    
   </div>
+</div>
 
 
 
