36821df8d2409dd05373e8eabde929724c02642d
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-20 14:55:44 -0500

    Create social profile links in page-about
    Hardcoded to match our mockup. I couldn't find an appropriate plugin to create this look.

--- page-about.php
+++ page-about.php
@@ -23,11 +23,18 @@
   <!-- Content Start -->
   <div class="container">
     <div class="row">
-      <div class="four columns responsive">
+      <div class="three columns responsive">
           <?php dynamic_sidebar('left-sidebar'); ?>
+          <h3>Contact Me</h3>
+            <a class="social-link email" href="mailo:moni@monibasu.com">Email Me</a>
+            <a class="social-link facebook" href="#">Facebook</a>
+            <a class="social-link twitter" href="#">Twitter</a>
+            <a class="social-link instagram" href="#">Instagram</a>
+            <a class="social-link linkedin" href="#">LinkedIn</a>
+          
         </div> 
 
-      <div class="eight columns">
+      <div class="nine columns">
         <?php
           if(have_posts()){
             while(have_posts()){
