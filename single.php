615ad46a3b1a54941bd9523061e4fe941f66b829
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-19 21:35:45 -0500

    Split main content row into 8 and 4 columns for sidebar

--- single.php
+++ single.php
@@ -1,26 +1,35 @@
 <?php get_header(); ?>
 
 <?php
-  if(have_posts()){
-    while(have_posts()){
-      the_post(); ?>
-      
-<!-- Hero Image -->      
-      <div id="hero" class="post-feature">
-            <?php the_post_thumbnail(); ?>
-      </div>
+if(have_posts()){
+  while(have_posts()){
+    the_post(); ?>
+    
+<!-- Hero Image -->
+        
+    <div id="hero" class="post-feature">
+      <?php the_post_thumbnail(); ?>
+    </div>
+    
+<!-- Title & Meta -->
 
-<!-- Title & Meta -->      
-      <div class="container">
-      <h3 class="post-title red"><?php the_title(); ?></h3>
-      <p class="pub-info post-date"><?php echo get_the_date(); ?> | <?php the_category(' | '); ?></p>
-
+    <div class="container">
+      <div class="row">
+        <div class="eight columns">
+        <h3 class="post-title red"><?php the_title(); ?></h3>
+        <p class="pub-info post-date"><?php echo get_the_date(); ?> | <?php the_category(' | '); ?></p>
+        
 <!-- Content Start -->
-      
-      <?php the_content(); ?>
-<?php    }//end of while loop
-  }//end if statement
- ?>
-
-</div>
- <?php get_footer(); ?>
+        
+        <?php the_content(); ?>
+      <?php    }//end of while loop
+    }//end if statement
+    ?>
+    </div>
+    <div class="four columns">
+    <h3>Test Sidebar</h3>
+    </div>
+  </div>
+  
+</div
+<?php get_footer(); ?>
