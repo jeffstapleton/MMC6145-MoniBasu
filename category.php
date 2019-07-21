f6b5e812b28940168b264306162622200e301806
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-21 14:09:14 -0500

    Change color and spacing of category.php

--- category.php
+++ category.php
@@ -5,17 +5,19 @@
     <h2 class="white center"><?php single_cat_title(); ?> Posts</h2>
   </div>
 </div>
-  <div class="row archive-row">
+  <div class="row">
     <?php 
     if(have_posts()){
       while(have_posts()){
         the_post();?>
         <div class="twelve columns archive spacer-tb">
+          <div class="archive-row">
           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
           <?php the_post_thumbnail('medium'); ?>
           <?php publish_author_date(); ?>
           <p><?php the_excerpt(); ?></p>
           <a href="<?php the_permalink(); ?>">Read More...</a>
+          </div>
         </div><?php
       }
     } 
