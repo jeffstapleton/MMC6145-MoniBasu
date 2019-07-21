181ff40b28229d0fcd2a85642fd936396ebd5347
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-21 18:15:23 -0500

    Pull php thumb out of inline css into regular wordpress hooks

--- blogpage2.php
+++ blogpage2.php
@@ -1,4 +1,4 @@
-<?php /*Template Name: Blog Page Template */ ?>
+<?php /*Template Name: Blog Page Template2 */ ?>
 
 <?php get_header(); ?>
 
@@ -22,24 +22,21 @@
 
 <div class="container">
   <div class="row spacer-tb">
-
-    <?php query_posts('showposts=2'); ?>
-    <?php $posts = get_posts('numberposts=5&offset=0'); foreach ($posts as $post) : start_wp(); ?>
-      <?php static $count1 = 0; if ($count1 == "2") { break; } else { ?>
-
-        <div class="one-half column posts">
-          <div class="article-home-feature-container">
-            <a href="<?php the_permalink(); ?>" rel="bookmark" class="article-home-feature-image" style="background-image:url(<?php echo get_the_post_thumbnail_url($post_id, 'large'); ?>);">
-            </a>
-          </div>
-              <h3 class="center"><a class="no-decor" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              <p><?php the_excerpt(); //the_content(); displays whole content?></p>
-              <a class="button" href="<?php the_permalink(); ?>">Read More >></a>
-            </div>
-
-        <?php $count1++; } ?>
-      <?php endforeach; ?>
+    <div class="one-half column">
+      <?php $catquery = new WP_Query( 'cat=3&posts_per_page=1' ); ?>
+      <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+        <div class="featured-block-image"></div>
+          <?php the_post_thumbnail_url($post_id, 'large'); ?>
+        </div>
+          <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+          <p><?php the_excerpt(); //the_content(); displays whole content?></p>
+        </div>  
+      <?php endwhile;
+      wp_reset_postdata();
+      ?>
+    
     </div>
+    
   </div>
 </div>
 
