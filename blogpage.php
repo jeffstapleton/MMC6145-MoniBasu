31f1d26331007f485858bb726b5b83a03a7d1eb8
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-21 14:32:40 -0500

    Add Hero code to blog page

--- blogpage.php
+++ blogpage.php
@@ -2,6 +2,24 @@
 
 <?php get_header(); ?>
 
+<!-- HERO SECTION -->
+<div id="hero">
+  <div class="row">
+    <div class="twelve columns">
+      <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+          <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
+          <?php
+        }
+      }
+      ?>
+      
+    </div>
+  </div>
+</div>
+
 <div class="container">
   <div class="row spacer-tb">
 
