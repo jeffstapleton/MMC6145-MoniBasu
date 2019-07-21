70d89ca52ce52219cae50b2d048ab181df61ca69
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-07-21 18:32:56 -0500

    Update indentation

--- blogpage2.php
+++ blogpage2.php
@@ -26,17 +26,18 @@
       <?php $catquery = new WP_Query( 'cat=3&posts_per_page=1' ); ?>
       <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
         <h3><?php echo the_category(); ?></h3>
-      <div class="blog-thumb"></div><?php the_post_thumbnail($post_id, 'medium'); ?>
-      <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-      <p><?php the_excerpt(); //the_content(); displays whole content?></p>
+        <div class="blog-thumb">
+          <?php the_post_thumbnail($post_id, 'medium'); ?>
+        </div>
+        <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+        <p><?php the_excerpt(); //the_content(); displays whole content?></p>
       </div>  
-      <?php endwhile;
-      wp_reset_postdata();
-      ?>
-    
-    </div>
-    
+    <?php endwhile;
+    wp_reset_postdata();
+    ?>
   </div>
+  
+</div>
 </div>
 
 <?php get_footer(); ?>
