d2466218b9f9f91d57703088c2bc007071e3d7cd
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-05 09:39:27 -0500

    undo all mobile hamburger attempts

--- header.php
+++ header.php
@@ -10,33 +10,21 @@
   </head>
   
   <body>
+    <nav class="desktop-menu">
     <nav id="custom-nav">
         <div id="logo">
           <?php if ( function_exists('the_custom_logo')) {
             the_custom_logo();
           } ?>
         </div>
-        <div class="nav-block desktop-menu">
+        <div class="nav-block">
           <?php wp_nav_menu(array(
             'theme_location' => 'header-menu',
             'container-class' => 'menu-header'
           ));
           ?>
         </div>
-      </nav>
-      <?php 
-      if ( function_exists('has_nav_menu') && has_nav_menu('mobile-menu') ) {
-    wp_nav_menu( array(
-      'depth' => 6,
-      'sort_column' => 'menu_order',
-      'container' => 'ul',
-      'menu_id' => 'main-nav',
-      'menu_class' => 'nav mobile-menu',
-      'theme_location' => 'mobile-menu'
-    ) );
-    } else {
-       echo "<ul class='nav mobile-menu'> <font style='color:red'>Mobile Menu has not been set</font> </ul>";
-} ?>
+      </nav>  
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-//Mobile Hamburger Nav
-function toggle() {
-    if ( jQuery( window ).width() >= 800 ) {
-      jQuery( '.nav.mobile-menu' ).hide();
-      jQuery( '.nav.desktop-menu' ).show();
-    } else {
-      jQuery( '.nav.desktop-menu' ).hide();
-      jQuery( '.nav.mobile-menu' ).show();
-    }
-}
-
-// on page load set the menu display initially
-toggle();
-
-// toggle the menu display on browser resize
-jQuery( window ).resize( function () {
-    toggle();
-} );
-
-
 //Nav background Tansition
 window.onscroll = () => {
   const nav = document.querySelector('#custom-nav');
