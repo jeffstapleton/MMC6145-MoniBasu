5349be602293be6d7d4bef8b959811dde1a1e6cc
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 18:29:06 -0500

    Update page-all.php

--- page-all.php
+++ page-all.php
@@ -1,59 +1,59 @@
 <?php /*Template Name: All Blog Posts Template*/ 
 get_header(); ?>
 
-  <!-- HERO SECTION -->
-  <div id="hero">
-    <div class="row">
-      <div class="twelve columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post(); ?>
-              <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
-              <?php
-            }
-          }
-        ?>
-      </div>
+<!-- HERO SECTION -->
+<div id="hero">
+  <div class="row">
+    <div class="twelve columns">
+      <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+          <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
+          <?php
+        }
+      }
+      ?>
     </div>
   </div>
-  
+</div>
+
 <div class="container">
   <div class="row">
     <div class="nine columns">
       <?php query_posts('posts_per_page=5');
       if(have_posts()) : 
         while(have_posts()) :
-           the_post(); ?>
-           <div class="row">
-           <div class="one-half column">
-             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
-           </div>
-           <div class="one-half column">
-           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-           
+          the_post(); ?>
+          <div class="row">
+            <div class="one-half column">
+              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+            </div>
+            <div class="one-half column">
+              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              
               <?php publish_author_date(); ?>
               <p><?php the_excerpt(); ?></p>
               <a href="<?php the_permalink(); ?>">Read More...</a> 
-              </div>
+            </div>
             
-                <?php
-    endwhile; 
-  endif; ?>
-</div>
-</div>
-</div>
-
-</div>
-      <div class="three columns">
-        <div class="sidebar"><?php dynamic_sidebar('blog-container-sidebar'); ?>
-        <h2>fart</h2>
-        </div> <!-- This gets the sidebar and inserts it here -->
+            <?php
+          endwhile; 
+        endif; ?>
       </div>
-
-
     </div>
-  </div>
+
+  
+
+<div class="three columns">
+  <div class="sidebar"><?php dynamic_sidebar('blog-container-sidebar'); ?>
+    <h2>fart</h2>
+  </div> <!-- This gets the sidebar and inserts it here -->
+</div>
+
+
+</div>
+</div>
 
 
 
