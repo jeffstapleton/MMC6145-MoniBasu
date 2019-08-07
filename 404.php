6a7909c614b1d701188a2c70de699e826f2807e9
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 12:31:59 -0500

    Auto Indent & Remove blank lines

--- 404.php
+++ 404.php
@@ -2,20 +2,17 @@
 
 get_header(); ?>
 <div class="error">
-<div class="container center-flex">
+  <div class="container center-flex">
     <div class="row four-info">
       <div class="twelve columns">
-
+        
         <h2>Whoops?!</h2>
         <h3 class="center">You followed a link to my page that doesn't exist or I haven't written about that topic yet.</h3>
         <p class="center">Go back to my <a href="<?php echo home_url('/'); ?>">home page</a></p>
-
+        
       </div>
     </div>
+  </div>
 </div>
-</div>
-
-
-
 
 <?php get_footer(); ?>
