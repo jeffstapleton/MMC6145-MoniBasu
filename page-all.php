d96f54c4dd6aa91c0406ae077bac0786867d1d8d
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 18:24:16 -0500

    Update column structure

--- page-all.php
+++ page-all.php
@@ -20,35 +20,35 @@ get_header(); ?>
   
 <div class="container">
   <div class="row">
+    <div class="nine columns">
       <?php query_posts('posts_per_page=5');
       if(have_posts()) : 
         while(have_posts()) :
            the_post(); ?>
-           <div class="three columns">
+           <div class="one-half column">
              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
            </div>
-           <div class="five columns">
+           <div class="one-half column">
            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
            
               <?php publish_author_date(); ?>
               <p><?php the_excerpt(); ?></p>
               <a href="<?php the_permalink(); ?>">Read More...</a> 
-            </div>
-            <?php
+              </div>
+                <?php
     endwhile; 
   endif; ?>
 
-
+</div>
       <div class="three columns">
         <div class="sidebar"><?php dynamic_sidebar('blog-container-sidebar'); ?>
         <h2>fart</h2>
         </div> <!-- This gets the sidebar and inserts it here -->
       </div>
-      </div>
 
 
     </div>
-
+  </div>
 
 
 
