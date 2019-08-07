69228f4233ea741ea50ffe5d24d1f1a2ac47af2c
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 17:49:39 -0500

    Create All Blog Posts page

---
+++ page-all.php
@@ -0,0 +1,43 @@
+<?php /*Template Name: All Blog Posts Template*/ 
+get_header(); ?>
+
+  <!-- HERO SECTION -->
+  <div id="hero">
+    <div class="row">
+      <div class="twelve columns">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post(); ?>
+              <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
+              <?php
+            }
+          }
+        ?>
+      </div>
+
+      <div class="nine columns single-content">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post(); ?>
+              <h2><?php the_title(); ?></h2>
+              <?php the_content();
+            }
+          }
+        ?>
+      </div>
+
+      <div class="three columns">
+        <div class="sidebar"><?php get_sidebar(); ?></div> <!-- This gets the sidebar and inserts it here -->
+      </div>
+
+
+    </div>
+  </div>
+
+
+
+
+
+<?php get_footer(); ?>
