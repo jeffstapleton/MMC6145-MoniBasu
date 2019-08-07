6c5401335dfc499c13c38aa7d13f87081b7a1b7b
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 17:54:29 -0500

    Update page-all

--- page-all.php
+++ page-all.php
@@ -15,11 +15,15 @@ get_header(); ?>
           }
         ?>
       </div>
-
+    </div>
+  </div>
+<div class="container">
+  <div class="row">
+    <div class="nine columns">
       <?php if(have_posts()){
         while(have_posts()){
           the_post();?>
-          <div class="twelve columns individual-post">
+          
             <h3><a href="<?php the_permalink() ?>"><?php
             the_title(); ?></a></h3>
             <p>Published on: <?php the_time('F j, Y'); ?></p>
@@ -29,7 +33,7 @@ get_header(); ?>
         }// ends while loop
       }// ends if statement
       ?>
-
+</div>
       <div class="three columns">
         <div class="sidebar"><?php get_sidebar(); ?></div> <!-- This gets the sidebar and inserts it here -->
       </div>
