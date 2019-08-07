3dc7e1080cd4da9c3cc71cc17006aa2fb3b2cb26
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 17:14:18 -0500

    Update hero home thumb to large

--- page-home.php
+++ page-home.php
@@ -10,7 +10,7 @@
       if(have_posts()){
         while(have_posts()){
           the_post(); ?>
-          <div class="page-feature"><?php the_post_thumbnail('medium'); ?></div>
+          <div class="page-feature"><?php the_post_thumbnail('large'); ?></div>
           <?php
         }
       }
