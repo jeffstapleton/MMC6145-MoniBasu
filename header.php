5c872cba25da66460364ae45912cc623d68da521
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 11:50:21 -0500

    convert .responsive-logo to #responsive-logo

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
       </nav>
     </nav>
     <div class="mobile-row">
-      <div class="responsive-logo"><?php if ( function_exists('the_custom_logo')) {
+      <div id="responsive-logo"><?php if ( function_exists('the_custom_logo')) {
         the_custom_logo();} ?>
       </div>
     </div>
