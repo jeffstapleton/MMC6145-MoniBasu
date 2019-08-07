faddf80d8e8918409057a7c02eded2a9423cc546
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 18:45:58 -0500

    Update page-all.php

--- page-all.php
+++ page-all.php
@@ -19,41 +19,38 @@ get_header(); ?>
 </div>
 
 <div class="container">
-    <div class="nine columns">
-      <?php query_posts('posts_per_page=5');
-      if(have_posts()) : 
-        while(have_posts()) :
-          the_post(); ?>
-          <div class="row archive spacer-tb">
-            <div class="one-half column">
-              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
-            </div>
-            <div class="one-half column">
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              
-              <?php publish_author_date(); ?>
-              <p><?php the_excerpt(); ?></p>
-              <a href="<?php the_permalink(); ?>">Read More...</a> 
-            </div>
+  <div class="nine columns">
+    <?php query_posts('posts_per_page=5');
+    if(have_posts()) : 
+      while(have_posts()) :
+        the_post(); ?>
+        <div class="row archive spacer-tb">
+          <div class="one-half column">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
           </div>
-            <?php
-          endwhile; 
-        endif; ?>
-      </div>
-
+          <div class="one-half column">
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            
+            <?php publish_author_date(); ?>
+            <p><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink(); ?>">Read More...</a> 
+          </div>
+        </div>
+        <?php
+      endwhile; 
+    endif; ?>
+  </div>
   
-
-<div class="three columns">
-  <div class="sidebar">
-    <?php dynamic_sidebar('blog-container-sidebar'); ?>
+  <div class="three columns">
+    <div class="sidebar">
+      <?php dynamic_sidebar('blog-container-sidebar'); ?>
+    </div>
   </div>
 </div>
-
-
-</div>
 </div>
 
-
+<div class="spacer-tb">
+</div>
 
 
 
