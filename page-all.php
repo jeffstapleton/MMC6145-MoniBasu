0827e426187276f7b5b98d52d4be9c8453ec665a
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 18:04:20 -0500

    Update Loop Code

--- page-all.php
+++ page-all.php
@@ -20,10 +20,33 @@ get_header(); ?>
   
 <div class="container">
   <div class="row">
-    <div class="nine columns">
-      <?php query_posts('posts_per_page=5');
-      if (have_posts()) : while (have_posts()) : the_post();
-      the_content();
+        <?php query_posts('posts_per_page=10');
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <div class="nine columns archive spacer-tb">
+              <div class="archive-row">
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <?php the_post_thumbnail('small'); ?>
+                <?php publish_author_date(); ?>
+                <p><?php the_excerpt(); ?></p>
+                <a href="<?php the_permalink(); ?>">Read More...</a>
+            <?php
+              endwhile;
+            endif; ?>
+            </div>
+            </div><?php
+          }
+        } 
+        ?>
+      </div>
+      
+      
+      <?php query_posts('posts_per_page=10');
+      if (have_posts()){
+        while (have_posts()){
+          the_post();
+      excerpt();
     endwhile; 
   endif; 
   ?>
