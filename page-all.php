a4aa7732405795d49e8f909ce4e546f20e1d149f
Jeff Stapleton <jeffstapleton@ufl.edu>
2019-08-07 17:57:53 -0500

    Remove intro material from loop

--- page-all.php
+++ page-all.php
@@ -17,12 +17,10 @@ get_header(); ?>
       </div>
     </div>
   </div>
+  
 <div class="container">
   <div class="row">
     <div class="nine columns">
-      <?php if(have_posts()){
-        while(have_posts()){
-          the_post();?>
           
             <h3><a href="<?php the_permalink() ?>"><?php
             the_title(); ?></a></h3>
